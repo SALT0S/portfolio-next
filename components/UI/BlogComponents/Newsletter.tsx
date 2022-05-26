d59af08119883ef77ba7798d92164ef8ccdc5150
SALT0S <josesansalt@gmail.com>
2022-05-25 20:04:19 -0500

    Newsletter final version

--- components/UI/BlogComponents/Newsletter.tsx
+++ components/UI/BlogComponents/Newsletter.tsx
@@ -1,16 +1,18 @@
 export const Newsletter = () => {
   return (
-    <section className="container mx-auto my-10 max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="grid h-72 grid-cols-2 items-center gap-10 bg-black px-10 text-white">
-        <div className="">
-          <h3 className="text-5xl">Join my newsletter</h3>
+    <section className="container mx-auto my-10 max-w-7xl px-4 text-white sm:px-6 lg:px-8">
+      <div className="grid items-center gap-10 bg-black py-[15%] px-[6%] md:p-[6%] lg:grid-cols-2">
+        <div>
+          <h3 className="text-5xl font-semibold">Join my newsletter</h3>
           <p className="mt-5 text-lg text-neutral-200">
             Join my newsletter to get new posts before anyone else, I&apos;ll
             send you an email with links to all of the articles.
           </p>
         </div>
 
-        <div>input suscribe</div>
+        <div>
+          <h2>input suscribe</h2>
+        </div>
       </div>
     </section>
   );
