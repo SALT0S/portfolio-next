405fd68fcd711a77288ebc5e274ffb6c5ebbad92
SALT0S <josesansalt@gmail.com>
2022-05-26 17:29:39 -0500

    Fixing errors

--- components/UI/HomeComponents/Header.tsx
+++ components/UI/HomeComponents/Header.tsx
@@ -2,9 +2,9 @@ import LogoTuco from "../complements/LogoTuco";
 
 export const HeaderSection = () => {
   return (
-    <section className="container mx-auto my-10 grid max-w-7xl	 flex-row  gap-10 px-4 sm:px-6 md:grid-cols-2 lg:px-8">
-      <div className="relative h-screen">
-        <div className="absolute bottom-1/4 ">
+    <section className="container mx-auto my-10 grid max-w-7xl flex-row gap-10 px-4 sm:px-6 md:grid-cols-2 lg:px-8">
+      <div className="relative md:h-screen">
+        <div className="md:absolute md:bottom-1/4">
           <h1 className="text-5xl font-semibold">
             Software engineering student & web developer
           </h1>
