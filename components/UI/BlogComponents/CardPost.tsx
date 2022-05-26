c81e1c7f72f352dff774af2282b4391b9f738d8c
SALT0S <josesansalt@gmail.com>
2022-05-25 19:35:10 -0500

    Card post

--- components/UI/BlogComponents/CardPost.tsx
+++ components/UI/BlogComponents/CardPost.tsx
@@ -1,7 +1,74 @@
- 
+import Image from "next/image";
+import Tuco from "/public/img/FotoTuco.jpg";
+
 export const CardPost = () => {
-	return (
-		<div><h2>
-			CardPost</h2></div>
-	)
-}
+  return (
+    <section className="my-16 grid gap-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+      <div className=" mb-8 w-full">
+        <div className="relative h-80 w-full">
+          <Image
+            src={Tuco}
+            alt="foto del tuco"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+
+        <article className="p-5">
+          <p className="text-sm">April 14th, 2022</p>
+          <h2 className="my-2 text-2xl font-semibold">
+            USE YOUR DESIRES AS A MUSE
+          </h2>
+          <p className="">
+            The more we develop as a person, we realize that life is not so
+            simple, but is that muse that helps us to be a bett...
+          </p>
+        </article>
+      </div>
+
+      <div className="mb-8 w-full ">
+        <div className="relative h-80 w-full">
+          <Image
+            src={Tuco}
+            alt="foto del tuco"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+
+        <article className="p-5">
+          <p className="text-sm">April 14th, 2022</p>
+          <h2 className="my-2 text-2xl font-semibold">
+            USE YOUR DESIRES AS A MUSE
+          </h2>
+          <p className="">
+            The more we develop as a person, we realize that life is not so
+            simple, but is that muse that helps us to be a bett...
+          </p>
+        </article>
+      </div>
+
+      <div className="mb-8 w-full ">
+        <div className="relative h-80 w-full">
+          <Image
+            src={Tuco}
+            alt="foto del tuco"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+
+        <article className="p-5">
+          <p className="text-sm">April 14th, 2022</p>
+          <h2 className="my-2 text-2xl font-semibold">
+            USE YOUR DESIRES AS A MUSE
+          </h2>
+          <p className="">
+            The more we develop as a person, we realize that life is not so
+            simple, but is that muse that helps us to be a bett...
+          </p>
+        </article>
+      </div>
+    </section>
+  );
+};
--- components/UI/BlogComponents/Newsletter.tsx
+++ components/UI/BlogComponents/Newsletter.tsx
@@ -1,6 +1,6 @@
 export const Newsletter = () => {
   return (
-    <section className="container mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
+    <section className="container mx-auto my-10 max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="grid h-72 grid-cols-2 items-center gap-10 bg-black px-10 text-white">
         <div className="">
           <h3 className="text-5xl">Join my newsletter</h3>
