1c2bef23e0a57b461f17536c7e1216db62df87c1
SALT0S <josesansalt@gmail.com>
2022-05-27 03:06:35 -0500

    Wave desing & contact section

--- components/UI/HomeComponents/Contact.tsx
+++ components/UI/HomeComponents/Contact.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import CatGif from "/public/static/gif/CatGif.gif";
+
 export const ContactSection = () => {
   return (
     <section className="container mx-auto my-10 max-w-7xl px-4 sm:px-6 md:flex lg:px-8">
-      <div className="">
+      <div className="self-center">
         <h2 className="text-5xl">Let&apos;s make something great together.</h2>
         <p className="my-8 text-lg">
           If you are looking for my contact info you can find it on the next
@@ -9,7 +12,9 @@ export const ContactSection = () => {
           simple form.
         </p>
       </div>
-      <div className="w-5/6">imagen</div>
+      <div className="h-2/4 md:w-3/4">
+        <Image src={CatGif} layout="responsive" alt="Cat Gif" />
+      </div>
     </section>
   );
 };
