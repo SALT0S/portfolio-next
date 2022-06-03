a7ec5a4afe7bffdeeea5b2d99c5eaf6141b22388
SALT0S <josesansalt@gmail.com>
2022-06-02 20:25:21 -0500

    Post card carousel

--- components/UI/Blog/PostCardHome.tsx
+++ components/UI/Blog/PostCardHome.tsx
@@ -1,4 +1,9 @@
 import Image from "next/image";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import "swiper/css";
+
 import { toDate } from "../../../lib/format-date";
 import { IPost } from "../../../interfaces";
 
@@ -8,31 +13,51 @@ interface PostsProps {
 
 export const PostCardHome: React.FC<PostsProps> = ({ posts }) => {
   return (
-    <>
+    <Swiper
+      spaceBetween={30}
+      breakpoints={{
+        "@0.00": {
+          slidesPerView: 1,
+        },
+        "@0.75": {
+          slidesPerView: 1.3,
+        },
+        "@1.00": {
+          slidesPerView: 1.6,
+        },
+        "@1.25": {
+          slidesPerView: 2,
+        },
+        "@1.50": {
+          slidesPerView: 3,
+        },
+      }}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay]}
+      centeredSlides={true}
+    >
       {posts.data.map((post) => (
-        <div
-          key={post.attributes.slug}
-          className="mt-10 grid gap-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3"
-        >
-          <div className="w-full">
-            <div className="relative h-80 w-full">
-              <Image
-                src={post.attributes.image.data.attributes.url}
-                alt={post.attributes.title}
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-
-            <article className="p-5">
-              <p className="text-sm">{toDate(post.attributes.date)}</p>
-              <h2 className="my-2 text-2xl font-semibold">
-                {post.attributes.title}
-              </h2>
-            </article>
+        <SwiperSlide key={post.attributes.slug} className="mt-10">
+          <div className="relative h-80 w-full">
+            <Image
+              src={post.attributes.image.data.attributes.url}
+              alt={post.attributes.title}
+              layout="fill"
+              objectFit="cover"
+            />
           </div>
-        </div>
+
+          <article className="p-5">
+            <p className="text-sm">{toDate(post.attributes.date)}</p>
+            <h2 className="my-2 text-2xl font-semibold">
+              {post.attributes.title}
+            </h2>
+          </article>
+        </SwiperSlide>
       ))}
-    </>
+    </Swiper>
   );
 };
--- components/UI/HomeComponents/Blog.tsx
+++ components/UI/HomeComponents/Blog.tsx
@@ -8,6 +8,7 @@ export const BlogSection: React.FC<PostsProps> = ({ posts }) => {
   return (
     <section className="container mx-auto my-10 max-w-7xl px-4 sm:px-6 lg:px-8">
       <h2 className="text-center text-5xl">Blog about personal opinions</h2>
+
       <PostCardHome posts={posts} />
     </section>
   );
