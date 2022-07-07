1527dc6ffe6d682202472718b9e95006aee60b24
SALT0S <josesansalt@gmail.com>
2022-07-07 17:57:14 -0500

    Fixing svgs

--- components/UI/HomeComponents/Skill.tsx
+++ components/UI/HomeComponents/Skill.tsx
@@ -13,7 +13,7 @@ export const SkillSection: React.FC<SkillsProps> = ({ skills }) => {
       <svg
         viewBox="0 0 1440 175"
         xmlns="http://www.w3.org/2000/svg"
-        className="absolute bottom-[99%] w-full dark:fill-zinc-900 md:relative"
+        className="absolute bottom-[98%] w-full leading-[0] dark:fill-zinc-900 md:relative"
       >
         <path d="M0 48L40 69.3C80 91 160 133 240 133.3C320 133 400 91 480 69.3C560 48 640 48 720 53.3C800 59 880 69 960 58.7C1040 48 1120 16 1200 5.3C1280 -5 1360 5 1400 10.7L1440 16V176H1400C1360 176 1280 176 1200 176C1120 176 1040 176 960 176C880 176 800 176 720 176C640 176 560 176 480 176C400 176 320 176 240 176C160 176 80 176 40 176H0V48Z" />
       </svg>
